0225c186cb86767379f669e6edd99135cc68ab84
KarenKazaryan <karenjan00list.ru>
2024-11-22 16:53:31 +0300

    return def twink_page

--- main_page/views.py
+++ main_page/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 def main_page(request):
     return HttpResponse('<html><h1>Это большой текст заголовка</h1></html>')
 
-
+def twink_page(request):
+    return HttpResponse('Это вторая страница')
